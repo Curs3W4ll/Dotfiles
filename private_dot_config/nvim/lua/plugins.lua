6ba01c24f4a69c523f839d39e5ddecb1f92fb7d3
Corentin Hervaud <corentin.hervaud@deski.io>
2023-07-24 10:23:44 +0200

    Update neo-tree to version 3

--- private_dot_config/nvim/lua/plugins.lua
+++ private_dot_config/nvim/lua/plugins.lua
@@ -195,7 +195,7 @@ require("lazy").setup({
     -- Tree (files explorer)
     {
         "nvim-neo-tree/neo-tree.nvim",
-        branch = "v2.x",
+        branch = "v3.x",
         dependencies = {
             "nvim-lua/plenary.nvim",
             "nvim-tree/nvim-web-devicons",
