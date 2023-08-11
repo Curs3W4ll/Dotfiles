24853a942b6e91b41e1f77620414b1bbb599b1bf
Corentin Hervaud <corentin.hervaud@deski.io>
2023-08-11 15:05:27 +0200

    feat(nvim): Add LspSaga plugin for LSP UI #53

---
+++ private_dot_config/nvim/lua/plugins/lspsaga.lua
@@ -0,0 +1,55 @@
+return function()
+
+    require("lspsaga").setup({
+        code_action = {
+            show_server_name = true,
+            extend_gitsigns = true,
+            keys = {
+                quit = { "q", "<Esc>", "<C-c>" },
+            },
+        },
+        definition = {
+            keys = {
+                split = "<C-c>s",
+            },
+        },
+        diagnostic = {
+            show_code_action = true,
+            jump_num_shortcut = true,
+            extend_relatedInformation = true,
+            keys = {
+                quit = { "q", "<Esc>", "<C-c>" },
+            },
+        },
+        finder = {
+            default = "def+imp+ref",
+            methods = {
+                tyd = "textDocument/typeDefinition",
+            },
+            keys = {
+                quit = { "q", "<Esc>", "<C-c>" },
+                shuttle = "<C-c><Space>",
+                split = "s",
+            },
+        },
+        hover = {
+            open_link = "<C-c>o",
+            open_cmd = "!brave-browser",
+        },
+        lightbulb = {
+            enable = false,
+        },
+        rename = {
+            auto_save = true,
+            keys = {
+                quit = { "<C-c>", "<Esc>" },
+                select = "<Space>",
+            },
+        },
+        scroll_preview = {
+            scroll_down = "<C-c>n",
+            scroll_up = "<C-c>N",
+        },
+    })
+
+end
