5650b0275d2b72143416f95a5b912fe472b73f33
Curs3W4ll <corentin.hervaud@epitech.eu>
2024-01-28 04:42:53 -0500

    fix(neo-tree): Focus neotree window when showing it #49

--- private_dot_config/nvim/lua/keybindings/neo-tree.lua
+++ private_dot_config/nvim/lua/keybindings/neo-tree.lua
@@ -1,8 +1,8 @@
 local wk = require("which-key")
 
 wk.register({
-    ["<C-n>"] = { ":Neotree show toggle<CR>", "Toggle neo tree (files explorer)" },
-    ["<C-g>"] = { ":Neotree float git_status<CR>", "Show git status explorer" },
+    ["<C-n>"] = { ":Neotree reveal toggle left filesystem<CR>", "Toggle neo tree (files explorer)" },
+    ["<C-g>"] = { ":Neotree toggle float git_status<CR>", "Show git status explorer" },
 }, {
     mode = "n",
 })
