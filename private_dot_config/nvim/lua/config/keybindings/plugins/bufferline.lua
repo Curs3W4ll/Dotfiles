a7a832b6e2c385bf406c565e6642dc33759d31bf
Curs3W4ll <corentin.hervaud@epitech.eu>
2025-04-12 12:30:19 +0200

    feat(nvim bufferline): Add binding to close all buffers

--- private_dot_config/nvim/lua/config/keybindings/plugins/bufferline.lua
+++ private_dot_config/nvim/lua/config/keybindings/plugins/bufferline.lua
@@ -2,6 +2,14 @@ return {
   setup = function()
     local wk = require("which-key")
 
+    local function close_all()
+      for _, item in ipairs(require("bufferline").get_elements().elements) do
+        vim.schedule(function()
+          vim.cmd("bd " .. item.id)
+        end)
+      end
+    end
+
     wk.add({
       { "<leader>b", group = "Buffers" },
       { "<leader>bn", "<cmd>BufferLineCycleNext<cr>", desc = "Go to next buffer" },
@@ -30,12 +38,14 @@ return {
       { "<leader>bc<", "<cmd>BufferLineCloseLeft<cr>", desc = "Close left buffer" },
       { "<leader>bc>", "<cmd>BufferLineCloseRight<cr>", desc = "Close right buffer" },
       { "<leader>bco", "<cmd>BufferLineCloseOthers<cr>", desc = "Close other buffers" },
+      { "<leader>bca", close_all, desc = "Close all buffers" },
+      { "<leader>bq", "<cmd>Bdelete<cr>", desc = "Close current buffer" },
       -- Shortands
       { "<leader>k", "<cmd>BufferLineCycleNext<cr>", desc = "Go to next buffer" },
       { "<leader>j", "<cmd>BufferLineCyclePrev<cr>", desc = "Go to previous buffer" },
       { "<leader>>", "<cmd>BufferLineMoveNext<cr>", desc = "Move current buffer right" },
       { "<leader><", "<cmd>BufferLineMovePrev<cr>", desc = "Move current buffer left" },
-      { "<leader>bq", "<cmd>Bdelete<cr>", desc = "Close current buffer" },
+      { "<leader>bQ", close_all, desc = "Close all buffers" },
     })
   end,
 }
